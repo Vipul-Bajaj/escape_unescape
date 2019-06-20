856815bacb60056bf18c6bf864f28266bbb911e5
Bajaj <vbajaj2@ms.ds.uhc.com>
2019-06-20 17:13:22 +0530

    Added Pretty JSON Functionality

--- static/main.js
+++ static/main.js
@@ -26,3 +26,61 @@ function unescape(){
     input = input.replace(/\\0/g,"\0")
     document.getElementById("output").value = input
 }
+
+function pretty_json() {
+    document.getElementById("output_label").innerHTML = "Jsonified String"
+    var input = document.getElementById("input").value
+    console.log(JSON.stringify(input, null, '\t'))
+    json = input
+    var result = [''];
+    var resultLine = 0;
+    var indent = '';
+    for (var i = 0; i < json.length; ++i) {
+        var curChar = json[i];
+        if (curChar == '{' || curChar == '[') {
+            result[resultLine] += indent + curChar;
+            result.push('');
+            ++resultLine;
+            indent += '\t';
+            continue;
+        }
+        if (curChar == '}' || curChar == ']') {
+            result.push('');
+            ++resultLine;
+            indent = indent.replace(/\t/, '');
+            result[resultLine] += indent + curChar;
+            continue;
+        }
+        if (curChar == ',') {
+            result[resultLine] += curChar;
+            result.push('');
+            ++resultLine;
+            continue;
+        }
+        if (result[resultLine] == '') {
+            result[resultLine] += indent;
+        }
+        result[resultLine] += curChar;
+        if (curChar == ':') {
+            var nextChar = json[i + 1];
+            if (nextChar == '{' || nextChar == '[') {
+                result.push('');
+                ++resultLine;
+            }
+        }
+        continue;
+    }
+    var html = ""
+    for (var i=0;i<result.length;i++){
+        html+=result[i]+"\n"
+    }
+    document.getElementById("output").value = html
+}
+
+function clear_input(){
+    document.getElementById("input").value = ""
+}
+
+function use_as_input(){
+    document.getElementById("input").value = document.getElementById("output").value
+}
